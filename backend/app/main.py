996131f62cd525e1cd3c5610b0743f74372b2b29
reofficial <rdroy@up.edu.ph>
2025-02-02 18:18:01 +0800

    backend: can now connect to database. added a /customers waypoint that sends entire Customer collection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,41 @@
+import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
+from typing import List
 
-load_dotenv()       #Loads some constants from the .env file
+# uvicorn app.main:app --reload
+
+load_dotenv()
 app = FastAPI()
+MONGO_URI = os.getenv("MONGO_URI")
 
-@app.get("/main")
-def main():
-    return {"message": "main test successful"}
+client = AsyncIOMotorClient(MONGO_URI)
+database = client["Kaya_Kita"]
+collection = database["Customer"]
+
+class Customer(BaseModel):
+    _id: str
+    email: str
+    password: str
+    first_name: str
+    last_name: str
+    middle_initial: str
+    username: str
+    address: str
+    contact_number: str
+    
+@app.get("/customers", response_model=List[Customer])
+async def get_customers():
+    customers_cursor = collection.find()
+    customers = await customers_cursor.to_list(length=None)
+    return [Customer(**customer) for customer in customers]
+
+@app.get("/hello")
+async def hello():
+    return {"message": "hello"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
