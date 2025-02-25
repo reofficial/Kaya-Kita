e0f78078b8b42fdeb7236e05d76e05b4221e8309
ATGLopez <111474174+ATGLopez@users.noreply.github.com>
2025-02-25 15:00:26 +0800

    added gov widget tests

---
+++ frontend/kayakita_gov/test/widget_test.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+// import 'package:kayakita_gov/home.dart' as homepage;
+import 'package:kayakita_gov/login.dart' as loginpage;
+import 'package:kayakita_gov/main.dart';
+
+void main() {
+  group ('Onboarding screen:', () {
+    testWidgets('Verify Log-in button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp()); 
+
+      expect(find.text('Log-in'), findsOneWidget);
+    });
+
+    testWidgets('Verify Create account button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp()); 
+
+      expect(find.text('Create account'), findsOneWidget);
+    });
+  });
+
+  group ('Login screen: ', () {
+    testWidgets('User can enter email', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      await tester.enterText(find.byType(loginpage.CustomTextField).first, 'email@domain.com');
+      expect(find.text('email@domain.com'), findsOneWidget);
+    });
+
+    testWidgets('User can enter password', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      await tester.enterText(find.byType(TextField).last, 'validpassword');
+      expect(find.text('validpassword'), findsOneWidget);
+    });
+
+    testWidgets('Verify Forgot password button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      expect(find.widgetWithText(TextButton,'Forgot your password?'), findsOneWidget);
+    });
+
+    testWidgets('Verify Sign in button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      expect(find.widgetWithText(ElevatedButton,'Sign in'), findsOneWidget);
+    });
+
+    testWidgets('Verify Create account button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      expect(find.widgetWithText(TextButton,'Create a new account'), findsOneWidget);
+    });
+
+    testWidgets('Verify Forgot password button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      expect(find.textContaining('Or continue with'), findsOneWidget);
+    });
+
+    testWidgets('Verify Google button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      expect(find.image(AssetImage('assets/google.png')), findsOneWidget);
+    });
+
+    testWidgets('Verify Facebook button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      expect(find.image(AssetImage('assets/facebook.png')), findsOneWidget);
+    });
+
+    testWidgets('Verify Apple button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: loginpage.LoginScreen()));
+
+      expect(find.image(AssetImage('assets/apple.png')), findsOneWidget);
+    });
+  });
+}
